f5a93635f584e03219acd21205683a054a8eee24
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-15 12:20:55 +0100

    skill button component

---
+++ src/components/SkillButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SkillButton = ({name, number, bg}) => {
+    return (
+        <div>
+            <div className="skill" style={{background: bg}}>
+                <div className="skills-btn">
+                    <span>{number}</span> <span>{name}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SkillButton
